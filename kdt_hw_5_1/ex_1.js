1c6fd480acd66560b18fb41e60ef35fc7f2fadb0
wan <2jin_j@naver.com>
2022-07-19 20:22:27 +0900

    add 5_1 hw

---
+++ kdt_hw_5_1/ex_1.js
@@ -0,0 +1,31 @@
+const table = document.querySelector("table");
+const dateInput = document.querySelector("#date");
+const innerText = document.querySelector("#content");
+const btn = document.querySelector("button");
+
+let target = null;
+
+table.addEventListener("click", function (e) {
+    if (e.target.tagName === "P") {
+        // console.log(e.target.textContent);
+        const dateValue = e.target.textContent;
+        dateInput.value = dateValue;
+
+        // console.log(e.target.parentNode);
+        // e.target.parentNode.append(dateInput.value);
+        // console.log(e.target.parentNode);
+        target = e.target.parentNode;
+    }
+})
+
+function writeSchedule() {
+    const divEl = document.createElement("div");
+    divEl.textContent = innerText.value;
+    target.append(divEl);
+
+    divEl.addEventListener("click", function () {
+        divEl.remove();
+    })
+}
+
+
